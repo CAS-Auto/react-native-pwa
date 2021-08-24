bd40e8d441492969d16c18756be6d5abb7cbf21b
nearshoreffreeman <frank.freeman@nearshoretechnology.com>
2021-08-24 11:34:10 -0500

    Fixing the spinning loader behavior

--- App.js
+++ App.js
@@ -1,55 +1,50 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
   const [isLoading, setLoading] = useState(false);
-  
-const  LoadingIndicatorView =()=>{
+
+  const LoadingIndicatorView = () => {
+    return (
+      <View style={styles.activityIndicatorStyle}>
+        <ActivityIndicator color="black" size="large" />
+      </View>
+    )
+
+  }
   return (
-    <View style={styles.activityIndicatorStyle}>
-      <ActivityIndicator color="black" size="large"  />
-    </View>
-  )
-  
-}
-  return (
-  
-  
     <SafeAreaView style={styles.container} >
-       
-      <WebView 
-        style={{flex:1}}
+
+      <WebView
+        style={{ flex: 1 }}
         source={{ uri: 'https://casautodev.wpengine.com/' }}
         containerStyle={{
           width: '100%',
           height: 500,
-          
+
           backgroundColor: "white",
           flex: 1,
-      }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      startInLoadingState={true}
-
-      onLoadStart={(syntheticEvent) => {
-        setLoading(true);
-      }}
-      onLoadEnd={(syntheticEvent) => {
-        setLoading(false);
-      }}
-      renderLoading={isLoading?LoadingIndicatorView:''}
-      startInLoadingState={true}
-    
-      
-      
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        onLoadStart={() => {
+          setLoading(true);
+        }}
+        onLoadEnd={() => {
+          setLoading(false);
+        }}
+        renderLoading={isLoading ? LoadingIndicatorView : ''}
+        startInLoadingState={true}
       />
-    {isLoading && <LoadingIndicatorView />}
-      
+
+      {isLoading && <LoadingIndicatorView />}
+
     </SafeAreaView>
-    
-    
+
+
   );
 }
 
@@ -59,17 +54,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:20,
+    marginTop: 20,
   },
-  activityIndicatorStyle:{
-    flex:1,
-    position:'absolute',
-   margin:'auto',
-   left:0,
-   right:0,
-   top:0,
-   bottom:0,
-   justifyContent:'center'
+  activityIndicatorStyle: {
+    flex: 1,
+    position: 'absolute',
+    margin: 'auto',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center'
   }
-  
+
 });
